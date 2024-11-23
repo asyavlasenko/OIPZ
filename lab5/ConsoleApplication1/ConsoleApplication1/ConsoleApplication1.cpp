24b43d9a001e230ded10d2b8a095bb2a8121d2b7
asyaaaa <asyavlasenko.ukr@gmail.com>
2024-11-23 22:19:11 +0200

    1

---
+++ lab5/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+bool isValidInput(int n, int step) {
+    if (n < 1) {
+        cout << "Error! The value of n must be at least 1." << endl;
+        return false;
+    }
+    if (step <= 0) {
+        cout << "Error! Step size must be greater than 0." << endl;
+        return false;
+    }
+    return true;
+}
+
+double calculateY(double x, int n) {
+    double y = 0;
+    if (x < 0) {
+        for (int i = 1; i <= n - 1; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                y += (x - i + j);
+            }
+        }
+    }
+    else {
+        for (int i = 0; i <= n - 1; ++i) {
+            if (i != 0) {
+                y += x / i;
+            }
+        }
+    }
+    return y;
+}
+
+void saveResults(double x, double y, ofstream& outFile) {
+    cout << "x = " << x << ", y = " << y << endl;
+    outFile << "x = " << x << ", y = " << y << endl;
+}
+
+int main() {
+    int n, start, stop, step;
+    cout << "Enter the start and end of the interval, step size, and value of n: ";
+    cin >> start >> stop >> step >> n;
+
+    if (!isValidInput(n, step)) {
+        return 1;
+    }
+
+    ofstream outFile("results.txt");
+    if (!outFile.is_open()) {
+        cout << "Error! Could not open file for writing." << endl;
+        return 1;
+    }
+
+    for (double x = start; x <= stop; x += step) {
+        double y = calculateY(x, n);
+        saveResults(x, y, outFile);
+    }
+
+    outFile.close();
+    cout << "Results have been saved to results.txt!" << endl;
+
+    return 0;
+}
---
+++ lab5/ConsoleApplication1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,75 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\university\oipz\5 лб\lab5\ConsoleApplication1\ConsoleApplication1\ConsoleApplication1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		
+
+		TEST_METHOD(TestIsValidInput_ValidData)
+		{
+			try
+			{
+				
+				Assert::IsTrue(isValidInput(3, 1));  
+			}
+			catch (const std::exception& e)
+			{
+				Assert::Fail(L"Unexpected exception thrown.");
+			}
+		}
+
+		TEST_METHOD(TestIsValidInput_InvalidData)
+		{
+			try
+			{
+				
+				Assert::IsFalse(isValidInput(0, -1));  
+			}
+			catch (const std::exception& e)
+			{
+				Assert::Fail(L"Unexpected exception thrown.");
+			}
+		}
+
+		TEST_METHOD(TestCalculateY_PositiveX)
+		{
+			try
+			{
+				
+				double x = 3;
+				int n = 3;
+				double expected = 4.5;  
+				double actual = calculateY(x, n);
+				Assert::AreEqual(expected, actual, 0.001);
+			}
+			catch (const std::exception& e)
+			{
+				Assert::Fail(L"Unexpected exception thrown.");
+			}
+		}
+
+		TEST_METHOD(TestCalculateY_ZeroX)
+		{
+			try
+			{
+				
+				double x = 0;
+				int n = 3;
+				double expected = 0;  
+				double actual = calculateY(x, n);
+				Assert::AreEqual(expected, actual, 0.001);
+			}
+			catch (const std::exception& e)
+			{
+				Assert::Fail(L"Unexpected exception thrown.");
+			}
+		}
+	};
+}
